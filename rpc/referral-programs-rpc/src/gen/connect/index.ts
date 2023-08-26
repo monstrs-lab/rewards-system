a2713758d32bf22c414fa394a5c06ca1b2a60fda
Andrey Linko <AndreyLinko@gmail.com>
2023-08-26 15:39:00 +0300

    feat: init referral programs rpc

---
+++ rpc/referral-programs-rpc/src/gen/connect/index.ts
@@ -0,0 +1,2 @@
+export * from './tech/monstrs/referralprograms/v1alpha1/referral-programs.service_connect.js'
+export * from './tech/monstrs/referralprograms/v1alpha1/referral-programs.service_pb.js'
---
+++ rpc/referral-programs-rpc/src/gen/interfaces/index.ts
@@ -0,0 +1,1 @@
+export * from './tech/monstrs/referralprograms/v1alpha1/referral-programs.service_pb.js'
